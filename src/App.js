05396b61d50bed743c68880656b83aaf055e90e6
kevin <kelvnjogu@gmail.com>
2024-09-11 23:21:45 +0300

    Code realignment

--- src/App.js
+++ src/App.js
@@ -1,68 +1,170 @@
 import { useState } from "react";
-import { AddFriendForm } from "./AddFriendForm";
-import { FriendsList } from "./FriendsList";
-import { BillCalculation } from "./BillCalculation";
+
+// const friends = [
+//     {
+//         name: "John Doe",
+//         image: "https://mighty.tools/mockmind-api/content/human/65.jpg",
+//         balance: 0,
+//         id: "kevin",
+//     },
+//     {
+//         name: "Cecilia Thuku",
+//         image: "https://mighty.tools/mockmind-api/content/human/44.jpg",
+//         balance: -1,
+//         id: "cecilia",
+//     },
+//     {
+//         name: "Lenox Njogu",
+//         image: "https://mighty.tools/mockmind-api/content/human/57.jpg",
+//         balance: 1,
+//         id: "lenox",
+//     },
+// ];
 
 export default function App() {
-    return <SplitBillContainer />;
-}
-
-function SplitBillContainer() {
-    const [friend, setFriend] = useState([]);
-    const [debtor, setDebtor] = useState("");
-    const [debt, setDebt] = useState(0);
-    const [billOpen, setBillOpen] = useState(false);
-    const [selectedFriend, setSelectedFriend] = useState("");
-    const [globalSelfBill, setGlobalSelfBill] = useState("");
-    const [globalFrenBill, setGlobalFrenBill] = useState("");
-    const [selected, setSelected] = useState(null);
+    const [friends, setFriends] = useState([]);
+    const [openBill, setOpenBill] = useState(false);
 
     return (
-        <div className="container">
-            <div className="container-friends">
-                <FriendsList
-                    friend={friend}
-                    debtor={debtor}
-                    onBillOpen={setBillOpen}
-                    billOpen={billOpen}
-                    setSelectedFriend={setSelectedFriend}
-                    globalSelfBill={globalSelfBill}
-                    globalFrenBill={globalFrenBill}
-                    selected={selected}
-                    setSelected={setSelected}
-                />
-                <AddFriendForm
-                    onSetFriend={setFriend}
-                    friend={friend}
-                    debtor={debtor}
-                    debt={debt}
-                />
-                <CloseButton />
-            </div>
-            <BillCalculation
-                billOpen={billOpen}
-                selectedFriend={selectedFriend}
-                setDebtor={setDebtor}
-                selfGlbBill={setGlobalSelfBill}
-                frenGlbBill={setGlobalFrenBill}
-                selected={selected}
-                setSelected={setSelected}
-                friend={friend}
-                setDebt={setDebt}
-                setFriend={setFriend}
-            />
+        <div className="app">
+            <FriendsList friends={friends} onBill={setOpenBill} />
+            <SplitBill openBill={openBill} />
+            <AddFriend onSetFriend={setFriends} />
         </div>
     );
 }
 
-function CloseButton() {
-    return <div className="close-btn">Close button</div>;
+function FriendsList({ friends, onBill }) {
+    return (
+        <div>
+            <ul className="friends">
+                {friends.map((friend, idx) => (
+                    <Friend
+                        friend={friend}
+                        key={friend.id}
+                        num={friend.id}
+                        onBill={onBill}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export function Button({ children }) {
+function Friend({ friend, onBill }) {
+    function handleSelection() {
+        onBill((bill) => !bill);
+    }
+
     return (
-        <div className="btn-box">
-            <button className="btn">{children}</button>
+        <li className="friend">
+            <img src={friend.image} alt={friend.name} className="img" />
+            <div className="profile-data">
+                <h2>{friend.name}</h2>
+                <p>
+                    {friend.balance === 0 && `You and ${friend.name} are even`}
+                </p>
+                <p className="owed">
+                    {friend.balance < 0 &&
+                        `${friend.name} owes you ${friend.balance} £`}
+                </p>
+                <p className="owe">
+                    {friend.balance > 0 &&
+                        `You owe ${friend.name} ${friend.balance} £`}
+                </p>
+            </div>
+            <button className="btn" onClick={handleSelection}>
+                Select
+            </button>
+        </li>
+    );
+}
+
+function AddFriend({ onSetFriend }) {
+    const [isOpen, setIsopen] = useState(false);
+    const [name, setName] = useState("");
+    const [url, setUrl] = useState("");
+
+    function toggleForm() {
+        setIsopen((isOpen) => !isOpen);
+    }
+
+    function createFriend(e) {
+        e.preventDefault();
+
+        if (!name || !url) return;
+        const newFriend = { name, url, balance: 0, id: crypto.randomUUID() };
+        onSetFriend((friends) => [...friends, newFriend]);
+
+        setName("");
+        setUrl("");
+    }
+
+    return (
+        <div>
+            {isOpen && (
+                <form className="form" onSubmit={(e) => createFriend(e)}>
+                    <div className="form-el">
+                        <label>Friend name</label>
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-el">
+                        <label>Image URL</label>
+                        <input
+                            type="url"
+                            value={url}
+                            onChange={(e) => setUrl(e.target.value)}
+                        />
+                    </div>
+                    <div className="btn-container">
+                        <button className="btn">Add</button>
+                    </div>
+                </form>
+            )}
+
+            <div className="btn-container">
+                <button className="btn" onClick={toggleForm}>
+                    {!isOpen ? `Add friend` : `close`}
+                </button>
+            </div>
+        </div>
+    );
+}
+
+function SplitBill({ openBill }) {
+    return (
+        <div className="form">
+            <h1>Split the Bill</h1>
+            {openBill && (
+                <form>
+                    <div className="form-el ">
+                        <label>Total bill</label>
+                        <input type="number" />
+                    </div>
+                    <div className="form-el ">
+                        <label>Your share</label>
+                        <input type="number" />
+                    </div>
+                    <div className="form-el ">
+                        <label>Friend share</label>
+                        <input type="number" />
+                    </div>
+                    <div className="form-el ">
+                        <label>Who is paying?</label>
+                        <select>
+                            <option value="you">You</option>
+                            <option value="friend">Friend</option>
+                        </select>
+                    </div>
+                    <div className="btn-container">
+                        <button className="btn">Add friend</button>
+                    </div>
+                </form>
+            )}
         </div>
     );
 }
